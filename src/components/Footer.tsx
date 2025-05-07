065325d18805a2e35285c58e6712b049b9034cd7
Coline OB <coline.cantelou@gmail.com>
2025-05-07 18:40:31 +0200

    color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,10 @@
 export default function Footer() {
   return (
-    <footer className="border-steel-gray-400 mt-20 border-t py-6 text-center text-sm text-gray-500">
+    <footer className="border-steel-gray-400 mt-20 border-t py-6 text-center text-sm text-gray-100">
       <p>© {new Date().getFullYear()} Coline Cantelou. Tous droits réservés.</p>
       <p className="mt-1 text-xs">
-        Design & développement 100% fait main avec <span className="text-neon">Next.js</span> &{' '}
-        <span className="text-neon">amour</span>.
+        Design & développement 100% fait main avec <span className="">Next.js</span> &{' '}
+        <span className="">amour</span>.
       </p>
     </footer>
   );
